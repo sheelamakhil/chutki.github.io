ddc45cbd8ed1faa97da41a94930ded78fd7a0e4a
sheelamakhil <121079760+sheelamakhil@users.noreply.github.com>
2023-02-11 00:46:35 +0530

    UPDATED

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const num1 = Math.ceil(Math.random()*1000)
-const num2 = Math.ceil(Math.random()*100)
+const num2 = Math.ceil(Math.random()*10)
 
 const questionEl = document.
 getElementById("question");
